897b198ab21f1356a9bfe39da12eee7a9b2071f6
Nitipat Wuttisasiwat <k-e-n_ken@hotmail.com>
2017-09-01 17:48:01 +0700

    index page and edit page success

--- db.js
+++ db.js
@@ -1,26 +1,79 @@
 var mysql      = require('mysql');
+var query = require('./query.js')
 var connection = mysql.createConnection({
   host     : '127.0.0.1',
   user     : 'root',
-  password : 'password',
+  password : '',
   database : 'crud_application',
   timeout: 60000
 });
 
+
 connection.connect((err) => {
-  if(!err) {
+  if(!err)
     console.log('Database connection success.');
-  }
-  else {
+  else
     console.log('Database connection error. \nerr:'+err);
-  }
 });
 
+connection.query(query.create_table_items, (err, results, fields) => {
+  if(err)
+    console.log(err);
+  else
+    console.log('Create TABLE items if not exists success.');
+});
+
+connection.query(query.create_table_users, (err, results, fields) => {
+  if(err)
+    console.log(err);
+  else
+    console.log('Create TABLE users if not exists success.');
+});
+
+exports.fetchData = (callback) => {
+  var err_response_1, err_response_2;
+  connection.query(query.insert_into_items, (err, results, fields) => {
+    err_response_1 = err;
+    if(err)
+      console.log(err);
+    else
+      console.log('Insert into items success.');
+  });
+
+  connection.query(query.insert_into_users, (err, results, fields) => {
+    err_response_2 = err;
+    if(err)
+      console.log(err);
+    else
+      console.log('Insert into users success.');
+  });
+
+  callback(err_response_1 && err_response_2);
+}
+
 exports.getItems = (callback) => {
-  connection.query('SELECT * from item', (err, results, fields) => {
-    if(err) {
-      console.log('query err');
-    }
+  connection.query('SELECT * from items;', (err, results, fields) => {
+    if(err)
+      console.log('Get Items Error.');
     callback(results);
   });
+}
+
+exports.getItemById = (item_id, callback) => {
+  connection.query('SELECT * from items WHERE item_id='+item_id+';', (err, results, fields) => {
+    if(err)
+      console.log('Get item by id error.');
+    callback(results);
+  });
+}
+
+exports.updateItem = (payload, callback) => {
+  var execute = "UPDATE items SET item_name = '"+payload.item_name+"', item_picture = '"+payload.item_picture+"', item_price = "+payload.item_price+", item_stock = "+payload.item_stock+" WHERE item_id = "+payload.item_id;
+  console.log(execute);
+  connection.query(execute,
+    (err, results, fields) => {
+      if(err)
+        console.log('Update Item Error.');
+      callback(err);
+    });
 }
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+var db = require('../db.js');
 var columns = [
   'ID',
   'Name',
@@ -8,45 +8,6 @@ var columns = [
   'Price',
   'Stock',
   'Action'
-]
-
-var columnn = [
-  {
-    "name": "ID",
-    "type": "text",
-    "key": "item_id",
-    "placholder": "Item ID"
-  },
-  {
-    "name": "Name",
-    "type": "text",
-    "key": "item_name",
-    "placholder": "Item Name"
-  },
-  {
-    "name": "Picture",
-    "type": "text",
-    "key": "item_picture",
-    "placholder": "Item Picture URL"
-  },
-  {
-    "name": "Price",
-    "type": "text",
-    "key": "item_price",
-    "placholder": "Item Price"
-  },
-  {
-    "name": "Stock",
-    "type": "text",
-    "key": "item_stock",
-    "placholder": "Item Stock Left"
-  },
-  {
-    "name": "Action",
-    "type": "bootstrap-button",
-    "key": "ActionButton",
-    "placholder": "ItemButton"
-  }
 ]
 
 var items = [
@@ -77,20 +38,24 @@ var title = "Items Stock | CRUD Web Application"
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index.ejs', { title: title, columns: columns, items: items, users: users });
+  db.getItems(results => {
+    res.render('index.ejs', { title: title, columns: columns, items: results });
+  });
 });
 
 router.get('/add', function(req, res, next) {
-  res.render('add.ejs', { title: title, columns: columns, items: items, users: users });
+  res.render('add.ejs', { title: title, columns: columns, items: items });
 });
 
 router.get('/edit/:item_id', function(req, res, next) {
   var item_id = req.params.item_id;
-  res.render('edit.ejs', { title: title, columns: columns, items: items, users: users });
+  db.getItemById(item_id, results => {
+    res.render('edit.ejs', { title: title, item: results[0] });
+  });
 });
 
 router.get('/layout', function(req, res, next) {
-  res.render('layout.ejs', { title: title, columns: columns, items: items, users: users });
+  res.render('layout.ejs', { title: title, items: items });
 });
 
 //API DATABASE REFERENCES
@@ -102,15 +67,19 @@ router.delete('/delete', function(req, res, next) {
 
 
 router.post('/edit', function(req, res, next) {
-  var item_id = req.body.item_id;
-  var item_name = req.body.item_name;
-  var item_picture = req.body.item_picture;
-  var item_price = req.body.price;
-  var item_stock = req.body.stock;
-  console.log('call edit item by id api.');
-  res.json({});
-
-
+  var payload = {
+    item_id : req.body.item_id,
+    item_name : req.body.item_name,
+    item_picture : req.body.item_picture,
+    item_price : req.body.item_price,
+    item_stock : req.body.item_stock
+ }
+ db.updateItem(payload, err => {
+   if(!err)
+     res.redirect('/');
+   else 
+     res.json({err: err});
+ });
 });
 
 router.post('/add', function(req, res, next) {
@@ -121,7 +90,15 @@ router.post('/add', function(req, res, next) {
   var item_stock = req.body.stock;
   console.log('call add item api.');
   res.json({});
+});
 
-})
+router.get('/fetch', function(req, res, next) {
+  db.fetchData(err => {
+    res.json({
+      msg: "fetch new data into database.",
+      success: err
+    });
+  });
+});
 
 module.exports = router;
